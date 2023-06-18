aa78b62e385c5b063abe337a0a2b25f8fa328ccf
Maximiliano Aguirre <maximiliano.aguirre@outlook.es>
2023-06-18 13:37:51 -0300

    cosmetic changes
    Signed-off-by: Maximiliano Aguirre <maximiliano.aguirre@outlook.es>

--- src/js/links.js
+++ src/js/links.js
@@ -1,7 +1,12 @@
-const links = document.querySelectorAll("#links>.card");
-
-links.forEach(link => {
+const linksCards = document.querySelectorAll("#links>.card");
+linksCards.forEach(link => {
     link.addEventListener("click", () => {
         window.open(link.dataset.link, "_blank")
     })
 });
+
+
+const links = document.querySelector("#links")
+links.addEventListener("click", (e) => {
+    document.elementsFromPoint(e.pageX, e.pageY).find(el => el.classList.contains("tile")).click()
+})
